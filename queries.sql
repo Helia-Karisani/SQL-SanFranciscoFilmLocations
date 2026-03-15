65932fe07bb425c415911133709e7588ec7b6fe1
Helia_Karisani <helia.karisani@gmail.com>
2026-03-14 23:25:21 -0400

    SELECT query

--- queries.sql
+++ queries.sql
@@ -1,3 +1,7 @@
 SELECT * FROM FilmLocations;
 
 SELECT Title, Director, Writer FROM FilmLocations;
+
+SELECT Title, ReleaseYear, Locations FROM FilmLocations WHERE ReleaseYear>=2001;
+
+SELECT FunFacts, Locations FROM FilmLocations;
